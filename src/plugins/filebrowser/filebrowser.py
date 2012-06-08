b593b78a76d75034b3d6a2569bb15c8cec6ac487
Micah Carrick <micah@quixotix.com>
2012-06-08 14:34:12 -0700

    Started plugin manager. Renamed add_dock_item to add_widget in window.

--- src/plugins/filebrowser/filebrowser.py
+++ src/plugins/filebrowser/filebrowser.py
@@ -5,20 +5,14 @@ from gi.repository import Gtk
 from mykiss.plugin import MykissPlugin
 from mykiss import icons
 
-STOCK_FILE_BROWSER = "mykiss-file-browser"
-
 class FileBrowserPlugin(MykissPlugin):
-
+    icon_name = Gtk.STOCK_DIRECTORY
     def activate(self):
         super(FileBrowserPlugin, self).activate()
-        
-        # register icons
-        icon_dir = os.path.join(os.path.dirname(__file__), "icons")
-        icons.register_stock_icons(icon_dir, (STOCK_FILE_BROWSER,))
-        
+
         # add dock item to existing windows
-        for window in self.application.get_windows():
-            self.add_dock_item_to_window(window)
+        #for window in self.application.get_windows():
+            #self.add_dock_item_to_window(window)
         
         # add dock item to existing windows
         self.connect(self.application, "window-added", self.on_window_added)
@@ -33,7 +27,7 @@ class FileBrowserPlugin(MykissPlugin):
         """
         widget = Gtk.TreeView()
         widget.show_all()
-        window.add_dock_item("File Browser", widget, STOCK_FILE_BROWSER, 
+        window.add_dock_item("File Browser", widget, self.icon_name, 
             Gtk.Orientation.VERTICAL)
         
     def deactivate(self):
--- src/plugins/terminal/terminal.py
+++ src/plugins/terminal/terminal.py
@@ -7,52 +7,47 @@ from gi.repository import Vte
 from mykiss.plugin import MykissPlugin
 from mykiss import icons
 
-STOCK_TERMINAL = "mykiss-terminal"
-
 class TerminalPlugin(MykissPlugin):
-
+    icon_name = "mykiss-terminal"
+    
     def __init__(self):
         super(TerminalPlugin, self).__init__()
+        # register icons
+        icon_dir = os.path.join(os.path.dirname(__file__), "icons")
+        icons.register_stock_icons(icon_dir, (self.icon_name,))
         
     def activate(self):
         super(TerminalPlugin, self).activate()
-        
-        # register icons
-        icon_dir = os.path.join(os.path.dirname(__file__), "icons")
-        icons.register_stock_icons(icon_dir, (STOCK_TERMINAL,))
-        
+
         # add terminal to existing windows
         for window in self.application.get_windows():
-            self.add_dock_item_to_window(window)
+            window.add_widget("Terminal", self._create_widget(), self.icon_name, 
+                              Gtk.Orientation.HORIZONTAL)
         
         # add terminal to any windows that get added
         self.connect(self.application, "window-added", self.on_window_added)
     
-    def add_dock_item_to_window(self, window):
-        """
-        Install a new terminal dock item in the window.
-        
-        Args:
-            window -- A `mykiss.window.Window()` instance.
-        
-        """
+    def _create_widget(self):
         vte = Terminal()
-        
-        #self._vte.connect("child-exited", self.on_child_exited)
         scrollbar = Gtk.Scrollbar.new(Gtk.Orientation.VERTICAL, 
             vte.get_vadjustment())
         box = Gtk.HBox(homogeneous=False, spacing=0)  
         box.pack_start(vte, True, True, 0)
         box.pack_start(scrollbar, False, False, 0)
         box.show_all()
-        window.add_dock_item("Terminal", box, STOCK_TERMINAL, 
-            Gtk.Orientation.HORIZONTAL)
+
+        return box
         
     def deactivate(self):
         super(TerminalPlugin, self).deactivate()
+        
+        # remove terminal from existing windows
+        for window in self.application.get_windows():
+            window.remove_widget("Terminal")
     
     def on_window_added(self, application, window, data=None):
-        self.add_dock_item_to_window(window)
+        window.add_widget("Terminal", self._create_widget(), self.icon_name, 
+                           Gtk.Orientation.HORIZONTAL)
 
 class Terminal(Vte.Terminal):
     __gtype_name__ = "MykissTerminal"
